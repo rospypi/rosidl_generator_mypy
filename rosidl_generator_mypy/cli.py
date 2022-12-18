a33e508e0c0438cb84c110a42b6dcaee4ed75bdf
Yuki Igarashi <me@bonprosoft.com>
2022-12-18 22:12:35 +0900

    Setup lint CI

--- rosidl_generator_mypy/cli.py
+++ rosidl_generator_mypy/cli.py
@@ -1,4 +1,5 @@
 import pathlib
+from typing import List
 
 from ament_index_python import get_package_share_directory
 from rosidl_cli.command.generate.extensions import GenerateCommandExtension
@@ -8,37 +9,39 @@ from rosidl_cli.command.translate.api import translate
 from rosidl_generator_mypy import generate
 
 
-class GenerateMypy(GenerateCommandExtension):
-
+class GenerateMypy(GenerateCommandExtension):  # type: ignore
     def generate(
         self,
-        package_name,
-        interface_files,
-        include_paths,
-        output_path
-    ):
-        generated_files = []
+        package_name: str,
+        interface_files: List[str],
+        include_paths: List[pathlib.Path],
+        output_path: pathlib.Path,
+    ) -> List[str]:
+        generated_files: List[str] = []
 
-        package_share_path = \
-            pathlib.Path(get_package_share_directory('rosidl_generator_mypy'))
-        templates_path = package_share_path / 'resource'
+        package_share_path = pathlib.Path(
+            get_package_share_directory("rosidl_generator_mypy")
+        )
+        templates_path = package_share_path / "resource"
 
         # Normalize interface definition format to .idl
-        idl_interface_files = []
-        non_idl_interface_files = []
+        idl_interface_files: List[str] = []
+        non_idl_interface_files: List[str] = []
         for path in interface_files:
-            if not path.endswith('.idl'):
+            if not path.endswith(".idl"):
                 non_idl_interface_files.append(path)
             else:
                 idl_interface_files.append(path)
         if non_idl_interface_files:
-            idl_interface_files.extend(translate(
-                package_name=package_name,
-                interface_files=non_idl_interface_files,
-                include_paths=include_paths,
-                output_format='idl',
-                output_path=output_path / 'tmp',
-            ))
+            idl_interface_files.extend(
+                translate(
+                    package_name=package_name,
+                    interface_files=non_idl_interface_files,
+                    include_paths=include_paths,
+                    output_format="idl",
+                    output_path=output_path / "tmp",
+                )
+            )
 
         # Generate code
         with legacy_generator_arguments_file(
@@ -46,7 +49,7 @@ class GenerateMypy(GenerateCommandExtension):
             interface_files=idl_interface_files,
             include_paths=include_paths,
             templates_path=templates_path,
-            output_path=output_path
+            output_path=output_path,
         ) as path_to_arguments_file:
             generated_files.extend(generate(path_to_arguments_file))
 
